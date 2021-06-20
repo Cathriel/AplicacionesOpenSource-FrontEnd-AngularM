c3b1e8a0acb52b51279496a9a21a1ede73c794d1
Luis F. Vasquez Solis Goycochea <vasquezluis_1304@hotmail.com>
2021-06-20 12:14:57 -0500

    Implementacion de la vista posterior al crear post correctamente

---
+++ roomies2/src/app/pages/successful-add/successful-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-successful-add',
+  templateUrl: './successful-add.component.html',
+  styleUrls: ['./successful-add.component.css']
+})
+export class SuccessfulAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
