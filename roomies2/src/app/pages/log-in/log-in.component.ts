4bbf0d84e100dc30fefe373290628d18ded4a511
Andreluis-Ingaroca <67808076+Andreluis-Ingaroca@users.noreply.github.com>
2021-06-20 12:30:11 -0500

    feat: log-in component added

---
+++ roomies2/src/app/pages/log-in/log-in.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css']
+})
+export class LogInComponent implements OnInit {
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
