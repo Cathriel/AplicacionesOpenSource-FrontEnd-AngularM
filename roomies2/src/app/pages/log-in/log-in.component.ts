744b8a7ff35076dfebae1fc111569f738b476e63
Andreluis-Ingaroca <67808076+Andreluis-Ingaroca@users.noreply.github.com>
2021-07-03 00:02:47 -0500

    fix: login final version

--- roomies2/src/app/pages/log-in/log-in.component.ts
+++ roomies2/src/app/pages/log-in/log-in.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from "@angular/forms";
+import {FormControl,FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {TokenStorageService} from "../../services/authentication/token-storage.service";
+import {Router} from "@angular/router";
+import { AuthService } from 'src/app/services/authentication/auth.service';
 
 @Component({
   selector: 'app-log-in',
@@ -7,6 +10,54 @@ import {FormControl, FormGroupDirective, NgForm, Validators} from "@angular/form
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
+  form!: FormGroup;
+  isLoggedIn=false;
+  isLoginFailed=false;
+  errorMessage='';
+  roles: string[] = [];
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authService: AuthService,
+              private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit():void {
+    this.form = this.formBuilder.group({
+      username: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]]
+    });
+    if(this.tokenStorageService.getToken()){
+      this.isLoggedIn=true;
+      this.roles=this.tokenStorageService.getUser().roles;
+    }
+  }
+  reloadPage():void{
+    window.location.reload();
+  }
+  onSubmit():void{
+    console.log("hola");
+    console.log(this.form.value);
+    console.log(this.form.value);
+    this.authService.login(this.form.value).subscribe(
+      data=>{
+        console.log(data);
+        this.tokenStorageService.saveToken(data.token);
+        this.tokenStorageService.saveUser(data);
+        this.isLoginFailed=false;
+        this.isLoggedIn=true;
+        this.roles=this.tokenStorageService.getUser().roles;
+        return this.router.navigate(['/']).then(()=>{
+          console.log(this.router.url);
+          window.location.reload();
+        });
+      },
+      error => {
+        console.log(error.error.errorMessage);
+        this.errorMessage = error.error.errorMessage;
+        this.isLoginFailed = true;
+        this.isLoggedIn = false;
+      }
+    )
+  }
 
   emailFormControl = new FormControl('', [
     Validators.required,
@@ -16,8 +67,6 @@ export class LogInComponent implements OnInit {
   passwordFormControl = new FormControl('', [
     Validators.required,
   ]);
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 }
+
